3d901c6aa14d4217cb123645c4d2b6559269675b
Atil Samancioglu <samancioglu.atil@gmail.com>
2017-06-25 12:18:17 +0300

    last commit

--- Segues/SecondViewController.swift
+++ Segues/SecondViewController.swift
@@ -10,26 +10,24 @@ import UIKit
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    var name = ""
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        nameLabel.text = "Name is: \(name)"
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func backClicked(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+        
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
--- Segues/ViewController.swift
+++ Segues/ViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    @IBOutlet weak var nameText: UITextField!
+    
+    var userName = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        if segue.identifier == "fromFirstToSecond" {
+            
+            let destinationVC = segue.destination as! SecondViewController
+            destinationVC.name = userName
+            
+        }
+        
+        
     }
 
+    @IBAction func saveClicked(_ sender: Any) {
+        userName = nameText.text!
+        performSegue(withIdentifier: "fromFirstToSecond", sender: nil)
+        
+    }
+    
 
 }
 
